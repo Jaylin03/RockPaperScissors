056f122016aae500133bd59156233b713c52e288
JaylinLeeSumlin <4368974-JaylinLeeSumlin@users.noreply.replit.com>
2023-12-04 15:22:47 +0000

    Complete 1 whole game, print user and computer moves, and print results for the round

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import random
 
-#initialize computer moves
-moves = ["rock" , "paper" , "scissors"]
-
-#pick computer moves
-pick = random.randint(0,2)
+#initialize and pick computer moves
+moves = ["Rock" , "Paper" , "Scissors"]
+compMove = moves[random.randint(0,2)]
 
 #user move
-user = int(input("Pick your move: \n1 = rock\n2 = paper\n 3 = scissors"))
+userPick = int(input("1 = Rock\n2 = Paper\n3 = Scissors\nPick your move (Enter a number): "))
+userMove = moves[userPick-1]
 
-if (user == moves[pick]):
+#print moves
+print ("\nCurrent Round:\n" + str(userMove) + " vs " + compMove)
+
+
+'''
+How to win:
+- rock > scissors --> 0 > 2
+- paper > rock --> 1 > 0
+- scissors > paper --> 2 > 1
+'''
+if (userMove == compMove):
   print ("Tie!")
-#elif ()
-
-#print computer move
-print (moves[pick])
+elif ((userMove == "Rock" and compMove == "Scissors") or (userMove == "Paper" and compMove == "Rock") or (userMove == "Scissors" and compMove == "Paper")):
+  print("You win!")
+else:
+  print("You lose! Try again next time.")
